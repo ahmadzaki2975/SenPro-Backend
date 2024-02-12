2a6434496da46449db5372c7deadb208f93d063d
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-02-12 20:16:46 +0700

    fix: wrong variable name

--- app.py
+++ app.py
@@ -5,19 +5,13 @@ import json
 
 from dotenv import load_dotenv
 load_dotenv()
-if not os.environ.get("OPEN_API_KEY"):
+print(os.environ.get("OPENAI_API_KEY"))
+if not os.environ.get("OPENAI_API_KEY"):
   raise ValueError("API Key not found.")  
 
 app = Flask(__name__)
 
 client = OpenAI()
-
-# assistant = client.beta.assistants.create(
-#     name="SmartAssistant",
-#     instructions="You are named SmartAssistant, made by Group 17 for their Senior Project. You will act as a smart customer service that can be personalized by each company for their needs by feeding you information through files or texts. Refrain from answering questions that may be beyond your role as a customer service.",
-#     tools=[],
-#     model="gpt-3.5-turbo"
-# )
 
 @app.route("/")
 def hello_world():
