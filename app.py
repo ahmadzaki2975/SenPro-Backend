cd082d8f821839ccee8b082a9e01ce2305242fdd
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-02-12 19:24:19 +0700

    feat: initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from openai import OpenAI
+import os
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+
+client = OpenAI()
+
+# assistant = client.beta.assistants.create(
+#     name="SmartAssistant",
+#     instructions="You are named SmartAssistant, made by Group 17 for their Senior Project. You will act as a smart customer service that can be personalized by each company for their needs by feeding you information through files or texts. Refrain from answering questions that may be beyond your role as a customer service.",
+#     tools=[],
+#     model="gpt-3.5-turbo"
+# )
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+@app.route("/chat", methods=['POST'])
+def chat(): 
+  prompt = request.json.get('prompt')
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "user", "content": prompt}
+    ]
+  )
+  print(completion.choices[0].message.content)
+  return completion.choices[0].message.content
