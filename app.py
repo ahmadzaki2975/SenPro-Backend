e756a273545bf6683edc869e5cc59ff01c7020bc
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-02-13 19:51:52 +0700

    feat: add route to chat personalized assistant

--- app.py
+++ app.py
@@ -12,14 +12,22 @@ if not os.environ.get("OPENAI_API_KEY"):
 app = Flask(__name__)
 
 client = OpenAI()
+assistant = client.beta.assistants.create(
+  name="SmartAssistance",
+  instructions="You are named SmartAssistant, made by Group 17 for their Senior Project. You will act as a smart customer service that can be personalized by each company for their needs by feeding you information through files or texts. Please refrain from answering questions that may be beyond your role as a customer service.",
+  model="gpt-3.5-turbo"
+)
 
 @app.route("/")
 def hello_world():
   return "<p>Hello, World!</p>"
 
-@app.route("/chat", methods=['POST'])
+@app.route("/chat", methods=["POST"])
 def chat(): 
-  prompt = request.json.get('prompt')
+  prompt = request.json.get("prompt")
+  if prompt is None:
+    return json.dumps({'error': 'No prompt provided'}), 400
+  
   completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -28,3 +36,22 @@ def chat():
   )
   print(completion.choices[0].message.content)
   return completion.choices[0].message.content
+
+@app.route("/chat/assistant", methods=["POST"])
+def chatAssistant():
+  prompt = request.json.get("prompt")
+  if prompt is None:
+    return json.dumps({'error': 'No prompt provided'}), 400
+  
+  completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": os.environ.get("ASSISTANT_INSTRUCTIONS")},
+    {"role": "user", "content": prompt}
+  ]
+  )
+  print(completion.choices[0].message.content)
+  return completion.choices[0].message.content
+  
+  
+  
